f4dba7062cbbafa9776383d117d93d064142fd0e
Mansi More <45144625+MansiSMore@users.noreply.github.com>
2022-01-30 00:00:12 +0530

    Update plus_minus.cpp

--- plus_minus.cpp
+++ plus_minus.cpp
@@ -26,13 +26,7 @@ int main()
 	cin >> iNo;
 
 	int *arr = new int[iNo];
-	//char *crr = (char*)&iNo;
-	//cout << sizeof(arr) << endl;	//Here, it will give you size of pointer.
-	
-	/*
-	You can't. The size of an array allocated with new[] is not stored in any way in which it can be accessed. Note 	that the return type of new [] is not an array - it is a pointer (pointing to the array's first element). So if 	you need to know a dynamic array's length, you have to store it separately.
-	*/
-	
+
 	cout << "Enter elements : \n";
 	for(int i = 0; i < iNo; i++)
 	{
